1365c5fbbbb4eea5d46b55644a7658d5d3fa67c3
leandro-cavalcante <leandro.cavalcante@cofinity-x.com>
2024-08-01 14:26:16 +0200

    feat: Created the base structure for the renewal credential job

---
+++ src/credentials/SsiCredentialIssuer.Renewal.App/Services/RenewalService.cs
@@ -0,0 +1,49 @@
+﻿/********************************************************************************
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace Org.Eclipse.TractusX.SsiCredentialIssuer.Renewal.App;
+
+/// <summary>
+/// Service to re-issue credentials that will expire in the day after.
+/// </summary>
+public class RenewalService
+{
+    private static readonly JsonSerializerOptions Options = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly ILogger<RenewalService> _logger;
+
+    public RenewalService(IServiceScopeFactory serviceScopeFactory,
+        ILogger<RenewalService> logger)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+        _logger = logger;
+    }
+
+    public async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        if (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("ADD IMPLEMENTATION TO RENEWAL THE CREDENTRIALS");
+        }
+    }
+}
