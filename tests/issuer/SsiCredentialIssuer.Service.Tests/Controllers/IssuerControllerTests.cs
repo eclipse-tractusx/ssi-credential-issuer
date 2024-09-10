44bd272e20c7da44954fd77ac30c3013ab2f6247
Phil Schneider <info@philschneider.de>
2024-09-10 15:10:58 +0200

    chore: remove test seeding files
    Refs: #205

--- tests/issuer/SsiCredentialIssuer.Service.Tests/Controllers/IssuerControllerTests.cs
+++ tests/issuer/SsiCredentialIssuer.Service.Tests/Controllers/IssuerControllerTests.cs
@@ -47,9 +47,10 @@ public class IssuerControllerTests(IntegrationTestFactory factory) : IClassFixtu
         var types = await _client.GetFromJsonAsync<IEnumerable<VerifiedCredentialTypeId>>($"{BaseUrl}/certificateTypes", JsonOptions);
 
         // Assert
-        types.Should().NotBeNull().And.HaveCount(2).And.Satisfy(
+        types.Should().NotBeNull().And.HaveCount(3).And.Satisfy(
             x => x == VerifiedCredentialTypeId.MEMBERSHIP,
-            x => x == VerifiedCredentialTypeId.BUSINESS_PARTNER_NUMBER
+            x => x == VerifiedCredentialTypeId.BUSINESS_PARTNER_NUMBER,
+            x => x == VerifiedCredentialTypeId.FRAMEWORK_AGREEMENT
         );
     }
 
